6b74be80e486b3ca8296ded89e2c7bac0dfabd84
jsiewierski11 <jarid.siewierski@gmail.com>
2020-07-07 12:20:59 -0600

    Moving register_function lines into one function

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ server = SimpleXMLRPCServer(('localhost', 3000), logRequests=True)
 def list_directory(dir):
     return os.listdir(dir)
 
-def do_some_dumb_task():
-    time.sleep(5)
-    return "done"
-
 def is_even(n):
     return n % 2 == 0
 
@@ -22,14 +18,16 @@ def hello_world():
 def wiki_summary(title, sentences=2, chars=0, auto_suggest=True, redirect=False):
     return wikipedia.summary(title, sentences, chars, auto_suggest, redirect)
 
-server.register_function(list_directory)
-server.register_function(is_even)
-server.register_function(hello_world)
-server.register_function(wiki_summary)
+def register_functions(server):
+    server.register_function(list_directory)
+    server.register_function(is_even)
+    server.register_function(hello_world)
+    server.register_function(wiki_summary)
 
 if __name__ == "__main__":
     try:
         print('Serving...')
+        register_functions(server)
         server.serve_forever()
     except KeyboardInterrupt:
         print("Exiting")
