8a59f1cbc470ecf96fd55feac0ed23ebbe15087c
jsiewierski11 <jarid.siewierski@gmail.com>
2020-07-07 12:03:04 -0600

    Creating simple wiki service

--- client_rpc2.py
+++ client_rpc2.py
@@ -3,4 +3,7 @@ from xmlrpc.client import ServerProxy
 proxy = ServerProxy('http://localhost:3000')
 
 if __name__ == "__main__":
-    print(proxy.do_some_dumb_task())
+    print("3 is even: {}".format(proxy.is_even(3)))
+    print("100 is even: {}".format(proxy.is_even(100)))
+
+    print("Wiki search of basketball: {}".format(proxy.wiki_summary("basketball")))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from xmlrpc.server import SimpleXMLRPCServer
 import os
 import time
+import wikipedia
+
+from xmlrpc.server import SimpleXMLRPCServer
 
 server = SimpleXMLRPCServer(('localhost', 3000), logRequests=True)
 
@@ -11,8 +13,19 @@ def do_some_dumb_task():
     time.sleep(5)
     return "done"
 
+def is_even(n):
+    return n % 2 == 0
+
+def hello_world():
+    print("hello world")
+
+def wiki_summary(title, sentences=2, chars=0, auto_suggest=True, redirect=False):
+    return wikipedia.summary(title, sentences, chars, auto_suggest, redirect)
+
 server.register_function(list_directory)
-server.register_function(do_some_dumb_task)
+server.register_function(is_even)
+server.register_function(hello_world)
+server.register_function(wiki_summary)
 
 if __name__ == "__main__":
     try:
