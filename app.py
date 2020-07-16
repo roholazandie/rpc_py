8287885ee083238c55754c21314a9febf076c534
jsiewierski11 <jarid.siewierski@gmail.com>
2020-07-16 15:05:38 -0600

    Trying to get flask and rpc to run in parallel

--- app.py
+++ app.py
@@ -3,22 +3,26 @@ import time
 import wikipedia
 import pymongo
 from pymongo import MongoClient, IndexModel
-from flask import Flask
-from xmlrpc.server import SimpleXMLRPCServer
+from flask import Flask, make_response, jsonify
+from xmlrpc.client import ServerProxy
 
 PORT = 8080
-app = Flask(__name__)
-server = SimpleXMLRPCServer(('localhost', 3000), logRequests=True)
+proxy = ServerProxy('http://localhost:3000')
 
 
-def wiki_summary(title, sentences=2, chars=0, auto_suggest=True, redirect=False):
-    print("here")
-    return wikipedia.summary(title, sentences, chars, auto_suggest, redirect)
+# def wiki_summary(title, sentences=2, chars=0, auto_suggest=True, redirect=False):
+#     print("here")
+#     return wikipedia.summary(title, sentences, chars, auto_suggest, redirect)
+
 
 if __name__ == "__main__":
-    print("Initiating Flask REST Service...")    
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    print("Initiating Flask REST Service...")  
+    app = Flask(__name__)  
+    
+    @app.route('/api/rest/v1.0/ask', methods=['POST'])
+    def ask():
+        response = make_response(jsonify({"response": proxy.wiki_summary("basketball")}))
+        print("Wiki search of basketball: {}".format(response))    
+        return None
 
-    server.register_function(wiki_summary)
-    print("Initiating XMLRPCSever...")
-    server.serve_forever()
+    app.run(host='0.0.0.0', port=PORT, debug=True)
